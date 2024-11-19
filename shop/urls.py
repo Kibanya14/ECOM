0d4ea388ed5f58e8db2ac89943612a99c7f312e2
Ir KIBANYA Jonas Landry Mobutu <jonasmasamba14@gmail.com>
2024-11-20 00:22:51 +0100

    configur pour deployement 3

--- shop/urls.py
+++ shop/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from . import views
+from . import views  # Assurez-vous que cela est correct
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('<int:myid>/', views.detail, name="detail"),
     path('checkout/', views.checkout, name="checkout"),
     path('confirmation/<int:order_id>/', views.confirmation, name="confirmation"),
-    path('download_invoice/<int:order_id>/', views.download_invoice, name="download_invoice"),  # Ajoutez cette ligne
+    path('download_invoice/<int:order_id>/', views.download_invoice, name="download_invoice"),  # Vérifiez ici
     path('about/', views.about, name='about'),
     path('forum/', views.forum, name='forum'),
 ]
